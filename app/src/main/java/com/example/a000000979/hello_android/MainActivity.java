bef90d3e92393b7a71f0ab5198c8f49e919f8110
yst8493182 <717041573@qq.com>
2016-11-09 09:17:50 +0800

    test

---
+++ app/src/main/java/com/example/a000000979/hello_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a000000979.hello_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
